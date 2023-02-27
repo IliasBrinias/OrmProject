cdecb7c903908b7c83685ce08b03c0a6756c2b45
IliasBrinias <iliasbrinias00@gmail.com>
2023-02-27 17:07:41 +0200

    Initial Commit

---
+++ src/test/java/com/unipi/msc/ormproject/OrmProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unipi.msc.ormproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
